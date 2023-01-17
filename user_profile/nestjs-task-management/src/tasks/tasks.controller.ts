a752ac632051a5408c9c92cb33877592de86baf8
Yushua <yushabakker1@gmail.com>
2023-01-17 16:57:07 +0100

    startign with saving data  locally, later it will be on a provider

--- user_profile/nestjs-task-management/src/tasks/tasks.controller.ts
+++ user_profile/nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    //define properties
+    constructor(private taskServices: TasksService) {}
+}
--- user_profile/nestjs-task-management/src/tasks/tasks.module.ts
+++ user_profile/nestjs-task-management/src/tasks/tasks.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 
+//the root, Module is always the root
 @Module({
   controllers: [TasksController],
+  providers: [TasksService],
 })
 export class TasksModule {}
