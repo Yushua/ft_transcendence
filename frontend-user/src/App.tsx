9ba9dcc9abaf87c3abf0fac10adb7b92644979dc
Yushua <yushabakker1@gmail.com>
2023-02-17 17:49:01 +0100

    added a post request,. of course I ned a get request later.

--- frontend-user/src/App.tsx
+++ frontend-user/src/App.tsx
@@ -1,7 +1,52 @@
 import React from 'react';
 import './App.css';
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
+type User = {
+  id: string;
+  name: string;
+  authenticationCode: string;
+}
+
+type GetUsersResponse = {
+  data: User[];
+}
+
+async function logIn() {
+  try {
+    // 👇️ const response: Response
+    const response = await fetch('http://localhost:4242/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+      },
+      Form-encoded: {
+
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    // 👇️ const result: GetUsersResponse
+    const result = (await response.json()) as GetUsersResponse;
+
+    console.log('result is: ', JSON.stringify(result, null, 4));
+
+    return result;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
+
+logIn();
 
 function App() {
   return (
@@ -12,3 +57,4 @@ function App() {
 }
 
 export default App;
+
