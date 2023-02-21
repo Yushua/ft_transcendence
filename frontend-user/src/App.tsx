ea1b9e17229db5685ade0eaf49ba85210eac3379
Yushua <yushabakker1@gmail.com>
2023-02-21 16:44:05 +0100

    get requests work, two different http request. one with if statements, the other without.

--- frontend-user/src/App.tsx
+++ frontend-user/src/App.tsx
@@ -1,16 +1,19 @@
 import React, { } from 'react';
 import './App.css';
 import fetch from 'node-fetch'
+
+
 async function logIn(username: string, password: string, email:string){
   console.log("sending message")
   try {
     // 👇️ const response: Response
-    const response = await fetch('http://localhost:4242/signup', {
+    const response = await fetch('http://localhost:4242/login/signup', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
+        'Content-Type': "application/x-www-form-urlencoded",
       },
-      body: `username=${username}&password=${password}eMail=${email}`
+      body: `username=${username}&password=${password}&eMail=${email}`
     });
 
     if (!response.ok) {
@@ -49,6 +52,24 @@ const handleFormSubmit = (e: React.FormEvent<YourFormElement>) => {
     e.currentTarget.elements.eMail.value);
 }
 
+// const response = await fetch('http://localhost:4242/signup', {
+//       method: 'POST',
+//       headers: {
+//         Accept: 'application/json',
+//       },
+//       body: `username=${username}&password=${password}eMail=${email}`
+
+function GetUsername(): string { return "AAAAAA" }
+function GetPassword(): string { return "aaAA!!11" }
+function GetEmail(): string { return "AAA@AAA.AAA" }
+
+function SendingStuff() {
+  var req = new XMLHttpRequest()
+  req.open("POST", 'http://localhost:4242/login/signup')
+  req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+  req.send(`username=${GetUsername()}&password=${GetPassword()}&eMail=${GetEmail()}`)
+}
+
 const App: React.FC = () => {
 
   return (
@@ -57,6 +78,7 @@ const App: React.FC = () => {
         <span className="heading">
           Login
         </span>
+        <button onClick={SendingStuff}>OI</button>
         <form onSubmit={handleFormSubmit}>
         <div>
           <label htmlFor="username">Username:</label>
