9b6fcffa8933681fc88273f1e226e7e3e2965881
LANIK2000 <lanikmail@gmail.com>
2023-03-06 14:34:23 +0100

    HTTP nicer syntax for headers.

--- frontend/src/Utils/HTTP.tsx
+++ frontend/src/Utils/HTTP.tsx
@@ -8,42 +8,42 @@ export default class HTTP {
 		}
 	}
 	
-	static Get(url: string, body: string | object | null = null, hdr: Map<string, string> | null = null): string
+	static Get(url: string, body: string | object | null = null, hdr: object | null = null): string
 		{ return this.SendRequest("GET", url, body, hdr) }
 	
-	static Post(url: string, body: string | object | null = null, hdr: Map<string, string> | null = null): string
+	static Post(url: string, body: string | object | null = null, hdr: object | null = null): string
 		{ return this.SendRequest("POST", url, body, hdr) }
 	
-	static Patch(url: string, body: string | object | null = null, hdr: Map<string, string> | null = null): string
+	static Patch(url: string, body: string | object | null = null, hdr: object | null = null): string
 		{ return this.SendRequest("PATCH", url, body, hdr) }
 	
-	static Delete(url: string, body: string | object | null = null, hdr: Map<string, string> | null = null): string
+	static Delete(url: string, body: string | object | null = null, hdr: object | null = null): string
 		{ return this.SendRequest("DELETE", url, body, hdr) }
 	
-	static async asyncGet(url: string, body: string | object | null = null, hdr: Map<string, string> | null = null, callback: ((msg: XMLHttpRequest) => any) | null = null, error: ((msg: XMLHttpRequest) => any) | null = null)
+	static async asyncGet(url: string, body: string | object | null = null, hdr: object | null = null, callback: ((msg: XMLHttpRequest) => any) | null = null, error: ((msg: XMLHttpRequest) => any) | null = null)
 		{ return this.asyncSendRequest("GET", url, body, hdr, callback, error) }
 	
-	static async asyncPost(url: string, body: string | object | null = null, hdr: Map<string, string> | null = null, callback: ((msg: XMLHttpRequest) => any) | null = null, error: ((msg: XMLHttpRequest) => any) | null = null)
+	static async asyncPost(url: string, body: string | object | null = null, hdr: object | null = null, callback: ((msg: XMLHttpRequest) => any) | null = null, error: ((msg: XMLHttpRequest) => any) | null = null)
 		{ return this.asyncSendRequest("POST", url, body, hdr, callback, error) }
 	
-	static async asyncPatch(url: string, body: string | object | null = null, hdr: Map<string, string> | null = null, callback: ((msg: XMLHttpRequest) => any) | null = null, error: ((msg: XMLHttpRequest) => any) | null = null)
+	static async asyncPatch(url: string, body: string | object | null = null, hdr: object | null = null, callback: ((msg: XMLHttpRequest) => any) | null = null, error: ((msg: XMLHttpRequest) => any) | null = null)
 		{ return this.asyncSendRequest("PATCH", url, body, hdr, callback, error) }
 	
-	static async asyncDelete(url: string, body: string | object | null = null, hdr: Map<string, string> | null = null, callback: ((msg: XMLHttpRequest) => any) | null = null, error: ((msg: XMLHttpRequest) => any) | null = null)
+	static async asyncDelete(url: string, body: string | object | null = null, hdr: object | null = null, callback: ((msg: XMLHttpRequest) => any) | null = null, error: ((msg: XMLHttpRequest) => any) | null = null)
 		{ return this.asyncSendRequest("DELETE", url, body, hdr, callback, error) }
 	
 	private static _setupRequest(
 			method: string,
 			url: string,
 			body: string | object | null,
-			hdr: Map<string, string> | null,
+			hdr: object | null,
 			detach: boolean)
 			: [XMLHttpRequest, string] {
 		var req = new XMLHttpRequest();
 		req.open(method, this.HostRedirect() + url, detach);
 		if (!!hdr)
-			for (const [key, value] of Array.from(hdr.entries()))
-				req.setRequestHeader(key, value)
+			for (const [name, value] of Object.entries(hdr))
+				req.setRequestHeader(name, value)
 		var fianlBody = ""
 		if (!!body)
 			switch (typeof body) {
@@ -62,7 +62,7 @@ export default class HTTP {
 			method: string,
 			url: string,
 			body: string | object | null,
-			hdr: Map<string, string> | null)
+			hdr: object | null)
 			: string {
 		var [req, fianlBody] = this._setupRequest(method, url, body, hdr, false)
 		req.send(fianlBody);
@@ -76,7 +76,7 @@ export default class HTTP {
 			method: string,
 			url: string,
 			body: string | object | null,
-			hdr: Map<string, string> | null,
+			hdr: object | null,
 			callback: ((msg: XMLHttpRequest) => any) | null,
 			error: ((msg: XMLHttpRequest) => any) | null) {
 		var [req, fianlBody] = this._setupRequest(method, url, body, hdr, true)
