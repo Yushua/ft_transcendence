086183a8ccf6063f869cfc6f2706557d2d068d3b
LANIK2000 <lanikmail@gmail.com>
2023-03-13 15:57:54 +0100

    Authentication now updates user correctly.

--- backend/src/auth/JwtStrategy.ts
+++ backend/src/auth/JwtStrategy.ts
@@ -21,17 +21,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         })
     }
     
-    async validate(req: Request, payload: JwtPayload): Promise<UserProfile> {
-        const { userID } = payload;
-        const id = userID
-        console.log("I am in validate")
-        const user: UserProfile = await this.autEntityRepos.findOneBy({ id });
-
-        if (!user){
-            throw new UnauthorizedException();
-        }
-        req["user"] = user
-        return user;
+    async validate(req: Request, payload: JwtPayload): Promise<boolean> {
+        // console.log(payload)
+        return true;
     }
-
 }
