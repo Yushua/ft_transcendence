53f9e2064207752e872f7f9d84a35a31f6a8b12c
Yushua <yushabakker1@gmail.com>
2023-03-16 14:36:41 +0100

    def mode

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -16,26 +16,26 @@ export class AuthController {
     }
 
     @Get('login')
-    @Redirect(`https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-c73b865f02b3cf14638e1a50c5caa720828d13082db6ab753bdb24ca476e1a4c&redirect_uri=http%3A%2F%2Flocalhost%3A4242%2Fauth%2Ftoken&response_type=code`,
+    @Redirect(`https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-c73b865f02b3cf14638e1a50c5caa720828d13082db6ab753bdb24ca476e1a4c&redirect_uri=http%3A%2F%2Flocalhost%3A4243%2F&response_type=code`,
         301
     )
     redirect() {}
 
     @Get('token/:code')
-    async getAuthToken(@Param('code') code: string, @Request() request, @Response() response):Promise<Response>{
+    async getAuthToken(@Param('code') code: string, @Request() request, @Response({ passthrough: true }) response):Promise<Response>{
         console.log("I am in there backend")
         const dataToPost = {
             grant_type: 'authorization_code',
             client_id: 'u-s4t2ud-c73b865f02b3cf14638e1a50c5caa720828d13082db6ab753bdb24ca476e1a4c',
             client_secret: `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`,
             code: code,
-            redirect_uri: "http://localhost:4242/",
+            redirect_uri: "http://localhost:4243/",
             state: " super-secret",
         }
         // console.log(`intraname works == ${this.AuthService.OauthSystemCodeToAccess(request, response, dataToPost)}`)
         console.log(`\n\n${code}`);
 
         response["intraname"] = await this.AuthService.OauthSystemCodeToAccess(request, response, dataToPost)
-        response.sending()
+        return response.send()
     }
 }
--- frontend/src/Login/LoginHandlerOAuth.tsx
+++ frontend/src/Login/LoginHandlerOAuth.tsx
@@ -16,10 +16,17 @@ async function setLogin(){
     console.log("checking the response if succesfull")
     console.log(`bmajor response ${response}`)
     console.log("i am before response")
+    // if (!response.ok) {
+    //   throw new Error(`Error! status: ${response.status}`);
+    // }
+    // const result = (await response.json());
+    const result = (await JSON.parse(response));
     console.log("result////")
+    console.log(result)
     newWindow(<MainWindow/>)
+    return result;
   } catch (error) {
-    console.log(`error in login ${error}`)
+    console.log(`error ${error}`)
     // alert("login in failed for some reason, back to login")
     // _setDisplay(false)
   }
@@ -41,8 +48,7 @@ async function setLogin(){
 
 const loginIntoOAuth = () => {
   console.log("i am here")
-  ///try go to
-  window.location.replace('https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-c73b865f02b3cf14638e1a50c5caa720828d13082db6ab753bdb24ca476e1a4c&redirect_uri=http%3A%2F%2Flocalhost%3A4242%2F&response_type=code');
+  window.location.replace('https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-c73b865f02b3cf14638e1a50c5caa720828d13082db6ab753bdb24ca476e1a4c&redirect_uri=http%3A%2F%2Flocalhost%3A4243%2F&response_type=code');
 }
 
 var _setDisplay:React.Dispatch<React.SetStateAction<boolean>>
@@ -60,7 +66,6 @@ function LoginHandlerOAuth(){
     }
   }
   // else if(Display == true){
-    //this sis et if the login failed
   //   setLogin();
   // }
 
