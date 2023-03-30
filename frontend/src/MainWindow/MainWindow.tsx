dc16b48c37e3de2b0cfb4e594f7612d1eaef3d32
Robin Drazsky <rdrazsky@f1r5s2.codam.nl>
2023-03-30 15:26:07 +0200

    MainWindow got a new AppBar.

--- frontend/src/MainWindow/MainWindow.tsx
+++ frontend/src/MainWindow/MainWindow.tsx
@@ -3,13 +3,16 @@ import { newWindow } from "../App";
 import MainChatWindow, { SetMainChatWindow } from "../Chat/Windows/MainChatWindow";
 import { Pong } from "../Games/pong/Pong";
 import TWTCheckPage from "../TwoFactorSystem/TWTCheckPage";
-import LogoutButtonComponent from "../UserProfile/ButtonComponents/LogoutButton";
+import LogoutButtonComponent, { logoutButtonRefresh } from "../UserProfile/ButtonComponents/LogoutButton";
 import SearchBar from "../UserProfile/Search bar/SearchBar";
 import SetUsername from "../UserProfile/SetUsername";
 import UserProfilePage from "../UserProfile/UserProfile";
 import User from "../Utils/Cache/User";
 import OurHistory from "../Utils/History";
 import HTTP from "../Utils/HTTP";
+import { AppBar, Box, Button, Container, IconButton, Toolbar, Tooltip } from "@mui/material";
+import ProfilePicture from "../UserProfile/ProfilePicture";
+import NameStorage from "../Utils/Cache/NameStorage";
 
 async function asyncGetName():Promise<string> {
 	const response = HTTP.Get(`user-profile/user`, null, {Accept: 'application/json'})
@@ -40,6 +43,8 @@ async function asyncToggleGetName(){
 	_setDisplay(true)
   };
 
+
+export const Width: number = Math.trunc(window.screen.width * .5)
 
 export default function MainWindow() {
 	const [currentWindow, setWindow] = useState<string>("")
@@ -73,32 +78,53 @@ export default function MainWindow() {
 		default: break
 	}
 	
+	const _buttonDistance = "2%"
 	return (
 		<center>
-			<div className={"MainWidnow"} style={{width: `${Math.trunc(window.screen.width * .5)}px`}}>
-				<div>
-					<LogoutButtonComponent />
-					<button
-						onClick={() => SetMainWindow("profile")}
-						disabled={currentWindow === "profile"}
-						>Profile</button>
-					<button
-						onClick={() => SetMainWindow("chat")}
-						disabled={currentWindow === "chat"}
-						>Chat</button>
-					<button
-						onClick={() => SetMainWindow("pong")}
-						disabled={currentWindow === "pong"}
-						>Play Pong</button>
-					<button
-						onClick={() => SetMainWindow("Search")}
-						disabled={currentWindow === "Search"}
-						>Search</button>
-					<button
-						onClick={() => SetMainWindow("TWTDisplay")}
-						disabled={currentWindow === "TWTDisplay"}
-						>TwoFactor</button>
-				</div>
+			<div className={"MainWidnow"} style={{width: `${Width}px`}}>
+				<AppBar position="static">
+					<Container maxWidth="xl">
+						<Toolbar disableGutters>
+							
+							{/* Logo */}
+							<Box
+								fontFamily={"'Courier New', monospace"}
+								fontSize={"200%"}>
+								Team-Zero
+							</Box>
+							
+							{/* Change Window Buttons */}
+							{[	["profile", "Profile"],
+								["chat", "Chat"],
+								["pong", "Play Pong"],
+								["Search", "Search"],
+								["TWTDisplay", "TwoFactor"],
+								].map(pair =>
+							<Box key={pair[0]} sx={{ pl:_buttonDistance }}>
+								<Button
+									sx={{ my: 2, color: 'white', display: 'block' }}
+									onClick={() => SetMainWindow(pair[0])}>
+										{pair[1]}
+								</Button>
+							</Box>)}
+							
+							{/* Logout Button */}
+							<Box sx={{ pl:_buttonDistance }}>
+								<Button sx={{ color: 'white', display: 'block' }}
+									onClick={() => logoutButtonRefresh()}>Logout
+								</Button>
+							</Box>
+							
+							{/* Avatar */}
+							<Box sx={{ position: "absolute", right: "0px" }}>
+								<IconButton sx={{ p: 0 }} onClick={() => SetMainWindow("profile")}>
+									<img src={User.ID !== "" ? HTTP.HostRedirect() + NameStorage.UserPFP.Get(User.ID) : ""}
+										style={{width: `${Width * .04}px`, height: `${Width * .04}px`, borderRadius: "50%"}}/>
+								</IconButton>
+							</Box>
+						</Toolbar>
+					</Container>
+				</AppBar>
 				{display}
 			</div>
 		</center>
