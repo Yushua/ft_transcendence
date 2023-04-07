858fc9b7b7045457a745e8f8741a145b78bfee07
Yushua <yushabakker1@gmail.com>
2023-04-07 12:39:54 +0200

    adding the stuff for the JWT, so authentication can work for Game-stat and

--- backend/src/achievements/achievements.module.ts
+++ backend/src/achievements/achievements.module.ts
@@ -5,11 +5,15 @@ import { Achievement } from './achievements.entity';
 import { AchievementService } from './achievements.service';
 import { UserProfile } from 'src/user-profile/user.entity';
 import { UserProfileAchievements } from 'src/relation_entities/user_profile_achievements.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Achievement, UserProfile, UserProfileAchievements])],
+	imports: [TypeOrmModule.forFeature([Achievement, UserProfile, UserProfileAchievements]),
+	PassportModule.register({ defaultStrategy: 'jwt'})],
+	
 	controllers: [AchievementController],
-	providers: [AchievementService]
+	providers: [AchievementService, JwtService]
 
 })
 export class AchievementsModule {}
--- backend/src/game-stats/game-stats.module.ts
+++ backend/src/game-stats/game-stats.module.ts
@@ -5,11 +5,14 @@ import { UserProfile } from 'src/user-profile/user.entity';
 import { GameStatsController } from './game-stats.controller';
 import { GameStatsService } from './game-stats.service';
 import { PongStats } from './pong-stats.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([UserProfile, UserProfilePongStats, PongStats])],
+	imports: [TypeOrmModule.forFeature([UserProfile, UserProfilePongStats, PongStats]),
+	PassportModule.register({ defaultStrategy: 'jwt'}),],
 	controllers: [GameStatsController],
-	providers: [GameStatsService]
+	providers: [GameStatsService, JwtService]
 
 })
 export class GameStatsModule {}
