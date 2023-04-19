86a04d3e27c34c9480490b70541a433f856049cc
mrAmyntas <basz_h@hotmail.com>
2023-04-19 15:59:46 +0200

    added ach play vs yourself

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -118,6 +118,8 @@ export class AuthService {
        ["Superb Showing", `https://i.imgur.com/LFMQ3tP.jpg`, "Win a game of Pong 11-0!"],
        ["Now you are a winner times ten!", `https://i.imgur.com/LFMQ3tP.jpg`, "Win 10 games of Pong!"],
        ["Busy Admin", `https://i.imgur.com/LFMQ3tP.jpg`, "Be a chat room admin with at least 10 people."],
+       ["You played yourself!", `https://i.imgur.com/LFMQ3tP.jpg`, "Play a game versus yourself!"],
+
      ]
       // async AllAchievements():Promise<string[][]>{
       //   return this.list
